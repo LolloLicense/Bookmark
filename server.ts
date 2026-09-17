6fc93969eb39dac84d46d292fc1df41cdaaa1f7e
LolloLicense <louise.sverkstrom@gmail.com>
2026-09-17 22:16:04 +0200

    adjusted port

--- server.ts
+++ server.ts
@@ -41,7 +41,8 @@ mongoose
   });
 
 // Start the express server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
